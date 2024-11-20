93e92d1d56cd2b1f4869f0a8d83983bdadd50c20
Mario Pérez <mp@mariopdev.com>
2024-11-20 01:18:40 +0100

    Initial Commit

---
+++ argo-workflows-app/argo_workflows_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  argo_workflows_appApp.swift
+//  argo-workflows-app
+//
+//  Created by Mario Pérez on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct argo_workflows_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ argo-workflows-appUITests/argo_workflows_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  argo_workflows_appUITestsLaunchTests.swift
+//  argo-workflows-appUITests
+//
+//  Created by Mario Pérez on 20/11/24.
+//
+
+import XCTest
+
+final class argo_workflows_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
